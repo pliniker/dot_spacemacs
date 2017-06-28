6bc69d9ce3aa462f13c85795deb58f2eeb069e63
pliniker <pliniker@buildfax.com>
2017-06-28 08:21:16 -0400

    Truncate lines

--- init.el
+++ init.el
@@ -284,7 +284,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-    (spacemacs/toggle-truncate-lines-off)
+    (spacemacs/toggle-truncate-lines-on)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
